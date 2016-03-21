33760df57a0f0936600201e19eca7d91b84a0f98
Raúl Uranga <ruranga@gmail.com>
2016-03-21 16:46:21 -0600

    update assertion module.

--- lib/macros/assertion.exs
+++ lib/macros/assertion.exs
@@ -1,5 +1,28 @@
 defmodule Assertion do
- defmacro assert({operator, _, [lhs, rhs]}) do
+ 
+  defmacro __using__(_options) do
+    quote do
+      import unquote(__MODULE__)
+      Module.register_attribute __MODULE__, :tests, accumulate: true
+      @before_compile unquote(__MODULE__)
+    end
+  end
+
+  defmacro __before_compile__(_env) do
+    quote do
+      def run, do: Assertion.Test.run(@tests, __MODULE__)
+    end
+  end
+
+  defmacro test(description, do: test_block) do
+    test_func = String.to_atom description
+    quote do
+      @tests {unquote(test_func), unquote(description)}
+      def unquote(test_func)(), do: unquote(test_block)
+    end
+  end
+  
+  defmacro assert({operator, _, [lhs, rhs]}) do
    quote bind_quoted: [operator: operator, lhs: lhs, rhs: rhs] do
      Assertion.Test.assert(operator, lhs, rhs)
    end
@@ -7,29 +30,43 @@ defmodule Assertion do
 end
 
 defmodule Assertion.Test do
-  
+   
+  def run(tests, module) do
+    Enum.each tests, fn {test_func, description} -> 
+      case apply(module, test_func, []) do
+        :ok -> IO.puts "."
+        {:fail, reason} -> IO.puts """
+        ===============================================
+                  FAILURE: #{description}
+        ===============================================
+                 #{reason}
+                 """
+      end
+    end
+  end
+
   def assert(:==, lhs, rhs) when lhs == rhs do
-    IO.write "."
+   :ok 
   end
   
   def assert(:==, lhs, rhs) do
-    IO.puts """
-      FAILURE: 
+    {:fail,  """
       Expected:       #{lhs}
       to be equal to: #{rhs}
-    """
+      """
+    }
   end
 
   def assert(:>, lhs, rhs) when lhs > rhs do
-    IO.write "."
+    :ok  
   end
   
   def assert(:>, lhs, rhs) do
-    IO.puts """
-      FAILURE:
+    {:fail, """
       Expected:          #{lhs}
       to be gratter than #{rhs}
-    """
+       """
+     }
   end
 
 end
