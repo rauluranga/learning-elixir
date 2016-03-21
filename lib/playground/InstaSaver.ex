41f2ca067500a369a22a68613bdd32411276d01d
Raúl Uranga <ruranga@gmail.com>
2016-03-21 11:57:21 -0600

    fix spaces.

--- lib/playground/InstaSaver.ex
+++ lib/playground/InstaSaver.ex
@@ -7,13 +7,20 @@ defmodule Playground.InstaSaver do
 	end
 
 	def parseInstagramURL(url) do
-    res = HTTPoison.get!(url)
-	  Floki.find(res.body, "[property='og:image']") |> Floki.attribute("content") |> List.first
+      
+      res = HTTPoison.get!(url)
+	  
+	  Floki.find(res.body, "[property='og:image']") 
+	    |> Floki.attribute("content") 
+	    |> List.first
+
 	end
 
 	def saveInstagramImage(image_path) do
-    image_req = HTTPoison.get!(image_path)
-    image_name = image_path |> String.split("?") |> List.first
+      image_req = HTTPoison.get!(image_path)
+      image_name = image_path 
+                     |> String.split("?")
+                     |> List.first
 	  File.write!("public/images/#{Path.basename(image_name)}", image_req.body)
 	end
 end
