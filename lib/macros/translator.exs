6acc1d6ec7ec5076ee2cea676ed990d66751c2a4
Raúl Uranga <ruranga@gmail.com>
2016-03-22 23:39:13 -0600

    added i18n sample

---
+++ lib/macros/translator.exs
@@ -0,0 +1,81 @@
+defmodule Translator do
+
+  defmacro __using__(_options) do
+    quote do
+      Module.register_attribute __MODULE__, :locales, accumulate: true, persist: false
+      import unquote(__MODULE__), only: [locale: 2]
+      @before_compile unquote(__MODULE__)
+    end
+  end
+
+  defmacro __before_compile__(env) do
+    compile(Module.get_attribute(env.module, :locales))
+  end
+
+  defmacro locale(name, mappings) do
+    quote bind_quoted: [name: name, mappings: mappings] do
+      @locales {name, mappings}
+    end
+  end
+
+  def compile(translations) do
+    translations_ast = for {locale, mappings} <- translations do
+      deftranslations(locale, "", mappings)
+    end
+
+    final_ast = quote do
+      def t(locale, path, binding \\ [])
+      unquote(translations_ast)
+      def t(_locale, _path, _bindings), do: {:error, :no_translation}
+    end
+    IO.puts Macro.to_string(final_ast)
+
+    final_ast
+  end
+
+  # deftranslations returns an AST for all dectionary keys, like the following:
+  # [[
+  #     [def(t("en", "flash.hello", bindings)) do
+  #     (((("" <> "Hello ") <> to_string(Dict.fetch!(bindings, :first))) <> " ") <> to_string(Dict.fetch!(bindings, :last))) <> "!"
+  #     end,
+  #     def(t("en", "flash.bye", bindings)) do
+  #     (("" <> "Bye, ") <> to_string(Dict.fetch!(bindings, :name))) <> "!"
+  #     end],
+  #     [def(t("en", "users.title", bindings)) do
+  #     "" <> "Users"
+  #     end]
+  # ]]
+  defp deftranslations(locale, current_path, mappings) do
+    for {key, val} <- mappings do
+      path = append_path(current_path, key)
+      if Keyword.keyword?(val) do
+        deftranslations(locale, path, val)
+      else
+        quote do
+          def t(unquote(locale), unquote(path), bindings) do
+            unquote(interpolate(val))
+          end
+        end
+      end
+    end
+  end
+
+  defp interpolate(string) do 
+    ~r/(?<head>)%{[^}]+}(?<tail>)/
+    |> Regex.split(string, on: [:head, :tail]) # will return an array like: ["Hello ", "%{first}", " ", "%{last}", "!"]
+    |> Enum.reduce "", fn
+      #one function with multiples bodies!!
+      #paterrn match arguments staring with %{
+      <<"%{" <> rest>>, acc ->
+        key = String.to_atom(String.rstrip(rest, ?}))
+        quote do 
+          unquote(acc) <> to_string(Dict.fetch!(bindings, unquote(key)))
+        end
+      #all other arguments
+      segment, acc -> quote do: (unquote(acc) <> unquote(segment)) 
+    end
+  end
+
+  defp append_path("", next), do: to_string(next)
+  defp append_path(current, next), do: "#{current}.#{next}"
+end
